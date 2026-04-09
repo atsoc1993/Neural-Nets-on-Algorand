974908c1feeac826c4012347521255bdc39b3bc6
atsoc1993 <atsoc1993@gmail.com>
2026-04-09 00:53:21 -0400

    Used a method of grouping training loops with near-max opcode budget possible to speed up training and prevent localnet node from collapsing. Should only error for epochs completion and threads cancel out when lock & event setting. No longer need to get current round, this was an attempt to fix the 1 tx group per round in --dev mode for localnet, --no-dev does not restrict tx count per round.

--- linear_regression/tests/4_train_model.py
+++ linear_regression/tests/4_train_model.py
@@ -1,17 +1,32 @@
 from constants import ALGORAND, LINEAR_REGRESSION_APP_ID, LINEAR_REGRESSION_MODEL_CLIENT
 from algokit_utils import CommonAppCallParams, AlgoAmount, SendParams
-from threading import Thread
+from threading import Thread, Event, Lock
+from time import sleep
+from random import randint
 import logging
-import sys
 
-def send_training_loop_tx(note_index: int, epoch: int):
+
+stop_event = Event()
+error_lock = Lock()
+
+def send_run_training_loops_tx(note_index: int, epoch: int):
+    if stop_event.is_set():
+        return
+    
     try:
-        current_round = ALGORAND.client.algod.suggested_params().first
-        txn_id = LINEAR_REGRESSION_MODEL_CLIENT.send.run_a_training_loop(
+        sleep_amount = randint(1, 400)
+        for i in range(sleep_amount):
+            sleep(1)
+            if stop_event.is_set():
+                return
+            
+        if stop_event.is_set():
+            return
+        
+        txn_id = LINEAR_REGRESSION_MODEL_CLIENT.send.run_training_loops(
             params=CommonAppCallParams(
-                max_fee=AlgoAmount(micro_algo=50_000),
+                max_fee=AlgoAmount(micro_algo=1_000_000),
                 note=str(note_index).encode(),
-                first_valid_round=current_round, # This should be able to be removed in testnet or mainnet, the assumption is block size is smaller by default on localnet
                 validity_window=1000 
             ),
             send_params=SendParams(
@@ -19,23 +34,24 @@ def send_training_loop_tx(note_index: int, epoch: int):
             )
         ).tx_id
         
-
-        logging.info(f"Ran Training Loop for Epoch: {epoch}, Tx ID: {txn_id}")
+        logging.info(f"Ran several Training Loops ... , Tx ID: {txn_id}")
     except Exception as e:
-        logging.error(f"{e}")
-        sys.exit()
+        with error_lock:
+            if not stop_event.is_set():
+                logging.error(f"Stopping all threads due to error on epoch {epoch}: {e}")
+                stop_event.set()
 
 globals = ALGORAND.app.get_global_state(LINEAR_REGRESSION_APP_ID)
 total_epochs = globals.get('epochs').value
 epochs_completed = globals.get('epochs_completed').value
 epochs = total_epochs - epochs_completed
 
-print(f'Epochs to complete')
+print(f'Epochs to complete: {epochs}')
 
 note_index = 1
 threads: list[Thread] = []
 for epoch in range(epochs):
-    thread = Thread(target=send_training_loop_tx, args=(note_index, epoch))
+    thread = Thread(target=send_run_training_loops_tx, args=(note_index, epoch))
     thread.start()
     threads.append(thread)
     note_index += 1
