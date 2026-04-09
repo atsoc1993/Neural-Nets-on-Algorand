a6e92b648bb226604f3fbdcdea1b2fd42035f3a0
atsoc1993 <atsoc1993@gmail.com>
2026-04-09 00:50:56 -0400

    Learning rate, scale factor, and epochs are now constants — scale factor is now also an arg for PrimeTraining method.

--- linear_regression/tests/3_prime_training_loops.py
+++ linear_regression/tests/3_prime_training_loops.py
@@ -1,14 +1,18 @@
 from LinearRegressionClient import PrimeTrainingArgs
-from constants import LINEAR_REGRESSION_MODEL_CLIENT, SCALE_FACTOR
+from constants import LINEAR_REGRESSION_MODEL_CLIENT, SCALE_FACTOR, LEARNING_RATE, EPOCHS
 import logging
 
-learning_rate = int(0.001 * SCALE_FACTOR)
-epochs = 10_000
 
+'''
+-- WARNING --
+IF YOU ADJUSTED THE SCALE FACTOR, NOTE THAT YOUR TRAINING DATA HAS ALREADY BEEN SCALED AND STORED
+YOU SHOULD DO A FULL RE-RUN w/ __full_run.py
+'''
 txn_ids = LINEAR_REGRESSION_MODEL_CLIENT.send.prime_training(
     args=PrimeTrainingArgs(
-        learning_rate=learning_rate,
-        epochs=epochs
+        scale_factor=SCALE_FACTOR,
+        learning_rate=LEARNING_RATE,
+        epochs=EPOCHS
     )
 ).tx_ids
 
